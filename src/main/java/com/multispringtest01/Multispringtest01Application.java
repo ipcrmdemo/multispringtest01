fe247cca428d7ae62b07e59c0e66ea04097ef5a5
Matt Cadorette <mcadorette@gmail.com>
2019-01-07 21:16:50 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/multispringtest01/Multispringtest01Application.java
@@ -0,0 +1,12 @@
+package com.multispringtest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Multispringtest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multispringtest01Application.class, args);
+	}
+}
---
+++ src/test/java/com/multispringtest01/Multispringtest01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.multispringtest01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Multispringtest01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
